ae6078ff7c73f6a041cbaac6a21c9df9726a921d
migrow <michael.growette@gmail.com>
2018-02-19 08:52:26 -0500

    filtered out the and a from beginning of id generator

--- dal.js
+++ dal.js
@@ -3,26 +3,29 @@ const PouchDB = require('pouchdb-core')
 PouchDB.plugin(require('pouchdb-adapter-http'))
 const db = new PouchDB(process.env.COUCHDB_URL)
 const slugster = require('slugify')
-const { reject, split, slice, startsWith } = require('ramda')
+const { reject, split, slice, join } = require('ramda')
 
 const createPainting = doc => {
-  // const docNameArray = split(' ', doc.name)
-  // console.log(docNameArray, slice(1, Infinity, docNameArray))
-  // if (docNameArray[0] === 'The' || docNameArray[0] === 'A') {
-  //   doc._id = `painting_${slugster(slice(1, Infinity, split(' ', doc.name)), {
-  //     lower: true
-  //   })}`
-  // } else {
-  //   doc._id = `painting_${slugster(doc.name, { lower: true })}`
-  // }
-  doc._id = `painting_${slugster(doc.name, { lower: true })}`
   doc.type = 'painting'
+  const splitDoc = split(' ', doc.name)
+  if (splitDoc[0] === 'The') {
+    doc._id = `${doc.type}_${slugster(join(' ', slice(1, Infinity, splitDoc)), {
+      lower: true
+    })}`
+  } else if (splitDoc[0] === 'A') {
+    doc._id = `${doc.type}_${slugster(join(' ', slice(1, Infinity, splitDoc)), {
+      lower: true
+    })}`
+  } else {
+    doc._id = `${doc.type}_${slugster(doc.name, { lower: true })}`
+  }
   return db.put(doc)
 }
 
 const createArtist = doc => {
-  doc._id = `artist_${slugster(doc.name, { lower: true })}`
   doc.type = 'artist'
+  doc._id = `${doc.tyoe}_${slugster(doc.name, { lower: true })}`
+
   return db.put(doc)
 }
 
