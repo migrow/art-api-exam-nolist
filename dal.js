80136e69ad75f2e915321eebc5a7addd398dde49
migrow <michael.growette@gmail.com>
2018-02-19 10:55:46 -0500

    moved id creator to pk-generator file

--- dal.js
+++ dal.js
@@ -4,28 +4,17 @@ PouchDB.plugin(require('pouchdb-adapter-http'))
 const db = new PouchDB(process.env.COUCHDB_URL)
 const slugster = require('slugify')
 const { reject, split, slice, join, pluck } = require('ramda')
+const pkGenerator = require('./lib/pk-generator')
 
 const createPainting = doc => {
   doc.type = 'painting'
-  const splitDoc = split(' ', doc.name)
-  if (splitDoc[0] === 'The') {
-    doc._id = `${doc.type}_${slugster(join(' ', slice(1, Infinity, splitDoc)), {
-      lower: true
-    })}`
-  } else if (splitDoc[0] === 'A') {
-    doc._id = `${doc.type}_${slugster(join(' ', slice(1, Infinity, splitDoc)), {
-      lower: true
-    })}`
-  } else {
-    doc._id = `${doc.type}_${slugster(doc.name, { lower: true })}`
-  }
+  pkGenerator(doc)
   return db.put(doc)
 }
 
 const createArtist = doc => {
   doc.type = 'artist'
-  doc._id = `${doc.tyoe}_${slugster(doc.name, { lower: true })}`
-
+  pkGenerator(doc)
   return db.put(doc)
 }
 
