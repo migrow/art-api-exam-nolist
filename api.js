df45ee8dc03dfbd0beb434eae2380f5dac7951b3
migrow <michael.growette@gmail.com>
2018-02-18 20:45:12 -0500

    required fields for artist and painting PUT and POST

--- api.js
+++ api.js
@@ -13,10 +13,47 @@ const {
   deleteArtist,
   updateArtist
 } = require('./dal')
+const reqFieldChecker = require('./lib/check-req-fields')
+const objCleaner = require('./lib/clean-body')
 
 const { propOr, join, not, isEmpty } = require('ramda')
 
 const port = propOr(4000, 'PORT', process.env.PORT)
+
+const paintingReqFieldChkr = reqFieldChecker([
+  'name',
+  'movement',
+  'artist',
+  'yearCreated',
+  'museum'
+])
+const artistReqFieldChkr = reqFieldChecker([
+  'name',
+  'style',
+  'country',
+  'birthYear',
+  'deathYear'
+])
+
+const putPaintingReqFields = reqFieldChecker([
+  '_id',
+  '_rev',
+  'name',
+  'movement',
+  'artist',
+  'yearCreated',
+  'museum'
+])
+
+const putArtistReqFields = reqFieldChecker([
+  '_id',
+  '_rev',
+  'name',
+  'style',
+  'country',
+  'birthYear',
+  'deathYear'
+])
 
 app.use(bodyParser.json())
 
@@ -25,12 +62,32 @@ app.get('/', function(req, res, next) {
 })
 
 app.post('/paintings', function(req, res, next) {
+  const missingFields = paintingReqFieldChkr(req.body)
+
+  if (not(isEmpty(missingFields))) {
+    next(
+      new HTTPError(
+        400,
+        `Missing Fields: ${join(' ', paintingReqFieldChkr(req.body))}`
+      )
+    )
+  }
   createPainting(req.body)
     .then(painting => res.send(painting))
     .catch(err => next(new HTTPError(err.status, err.message, err)))
 })
 
 app.post('/artists', function(req, res, next) {
+  const missingFields = artistReqFieldChkr(req.body)
+
+  if (not(isEmpty(missingFields))) {
+    next(
+      new HTTPError(
+        400,
+        `Missing Fields: ${join(' ', artistReqFieldChkr(req.body))}`
+      )
+    )
+  }
   createArtist(req.body)
     .then(artist => res.send(artist))
     .catch(err => next(new HTTPError(err.status, err.message, err)))
@@ -69,12 +126,60 @@ app.delete('/artists/:id', function(req, res, next) {
 })
 
 app.put('/paintings/:id', function(req, res, next) {
-  updatePainting(req.body)
+  if (isEmpty(req.body)) {
+    next(new HTTPError(400, 'Missing Request Body'))
+    return
+  }
+  const bodyCleaner = objCleaner([
+    '_id',
+    '_rev',
+    'name',
+    'movement',
+    'artist',
+    'yearCreated',
+    'museum'
+  ])
+  const cleanedBody = bodyCleaner(req.body)
+  const missingFields = putPaintingReqFields(cleanedBody)
+  if (not(isEmpty(missingFields))) {
+    next(
+      new HTTPError(
+        400,
+        `Request body missing these fields: ${join(', ', missingFields)}`
+      )
+    )
+    return
+  }
+  updatePainting(cleanedBody)
     .then(painting => res.send(painting))
     .catch(err => next(new HTTPError(err.status, err.message, err)))
 })
 
 app.put('/artists/:id', function(req, res, next) {
+  if (isEmpty(req.body)) {
+    next(new HTTPError(400, 'Missing Request Body'))
+    return
+  }
+  const bodyCleaner = objCleaner([
+    '_id',
+    '_rev',
+    'name',
+    'style',
+    'country',
+    'birthYear',
+    'deathYear'
+  ])
+  const cleanedBody = bodyCleaner(req.body)
+  const missingFields = putArtistReqFields(cleanedBody)
+  if (not(isEmpty(missingFields))) {
+    next(
+      new HTTPError(
+        400,
+        `Request body missing these fields: ${join(', ', missingFields)}`
+      )
+    )
+    return
+  }
   updateArtist(req.body)
     .then(artist => res.send(artist))
     .catch(err => next(new HTTPError(err.status, err.message, err)))
